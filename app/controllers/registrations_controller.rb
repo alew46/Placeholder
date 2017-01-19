0eaaf324725f421705d696dfbd7fb69a3186cda1
Alex Lewis <alew46@gmail.com>
2017-01-19 12:03:46 -0500

    configured redirects after sign in / up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -9,4 +9,15 @@ class RegistrationsController < Devise::RegistrationsController
   def account_update_params
     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
   end
+
+  # def after_sign_up_path_for(resource)
+  #   @user = current_user
+  #   @user
+  # end
+  #
+  # def after_inactive_sign_up_path_for(resource)
+  #   @user = current_user
+  #   @user
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
 
   devise_for :users, :controllers => { registrations: 'registrations' }
-  
+
   get 'home/index'
 
   get 'home/about'
 
   root 'home#index'
+
+  resources :users, only: [ :show ]
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

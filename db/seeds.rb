5bf1dc5f3e43d2aeb56bc52113f744b00f5848e5
Alex Lewis <alew46@gmail.com>
2017-01-20 14:20:57 -0500

    wrote CRUD actions for bookmarks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+10.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: Faker::Internet.password,
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+end
+
+User.create!(
+  email: "test@user.com",
+  password: "helloworld",
+  first_name: "Test",
+  last_name: "User"
+)
+
+users = User.all
+
+15.times do(
+  Topic.create!(
+    title: Faker::Lorem.word,
+    user: users.sample
+  )
+end
+
+topics = Topic.all
+
+50.times do
+  Bookmark.create!(
+    url: Faker::Internet.url,
+    topic: users.sample
+  )
+end
+
+puts "Seeding complete."
+puts "#{User.count} users created."
+puts "#{Topic.count} topics created."
+puts "#{Bookmark.count} bookmarks created."

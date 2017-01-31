5ee2341f27c794dc7f2aa3d23b7463297a515d06
Alex Lewis <alew46@gmail.com>
2017-01-31 10:13:29 -0500

    optimized images

--- app/helpers/bookmarks_helper.rb
+++ app/helpers/bookmarks_helper.rb
@@ -1,2 +1,9 @@
 module BookmarksHelper
+
+  def get_host_without_www(url)
+        url = "http://#{url}" if URI.parse(url).scheme.nil?
+        host = URI.parse(url).host.downcase
+        host.start_with?('www.') ? host[4..-1] : host
+  end
+
 end
--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,3 +1,27 @@
 class Bookmark < ActiveRecord::Base
   belongs_to :topic
+
+  before_save :normalize_url
+  after_create :set_image_url
+
+
+  def set_image_url
+    begin
+      object = LinkThumbnailer.generate(url)
+      Rails.logger.info "#{object.images.first.inspect}"
+      update_attribute(:image_url, object.images.first.to_s)
+    rescue
+      Rails.logger.warn "Bad image url for bookmark #{self.id}"
+    end
+  end
+
+  def normalize_url
+    self.url = "http://#{self.url}" if URI.parse(self.url).scheme.nil?
+    self.url = self.url
+  end
+
+  def pretty_url
+    host = URI.parse(url).host.downcase
+    host.start_with?('www.') ? host[4..-1] : host
+  end
 end
